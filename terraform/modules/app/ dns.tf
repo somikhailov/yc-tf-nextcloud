5846d9802ee7d0e09f1ac4876bdad667eb6324e7
Sergei Mikhailov <somikhaylov@yandex.ru>
2022-05-29 21:35:19 +0700

    add nextcloud deploy

--- terraform/modules/app/ dns.tf
+++ terraform/modules/app/ dns.tf
@@ -2,9 +2,9 @@ data "yandex_dns_zone" "zone" {
   name = var.dns_zone
 }
 
-resource "yandex_dns_recordset" "app" {
+resource "yandex_dns_recordset" "nextcloud" {
   zone_id = data.yandex_dns_zone.zone.id
-  name    = "app"
+  name    = "nextcloud"
   type    = "A"
   ttl     = 200
   data    = [yandex_compute_instance.vm.network_interface.0.nat_ip_address]
